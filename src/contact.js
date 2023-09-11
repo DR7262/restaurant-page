5ff2944b6e2ba01a1298bdff41d3276e18855c18
DR7262 <colina.rbm@gmail.com>
2023-09-11 12:52:02 +1000

    Created file for DOM functions, updated contact.js

--- src/contact.js
+++ src/contact.js
@@ -1,25 +1,21 @@
+import {createBoxText, createDOMElement} from "./domFunctions.js";
+
 const contactTitle = document.createElement('div');
 contactTitle.textContent = 'Contact Us';
 contactTitle.classList.add('contact')
 
-const contactBoxes = document.createElement('div');
-contactBoxes.classList.add('contactBoxes');
-
-function createBoxText(text) {
-    let varName = document.createElement('p')
-    varName.innerHTML = text;
-    varName.classList.add('boxText')
-    return varName;
-};
+const contactBoxes = createDOMElement('div', 'contactBoxes')
 
 const box1 = document.createElement('div');
 box1.classList.add('contactBox');
 
-box1.appendChild(createBoxText('Business Hours<br>Monday - Sunday<br>10:00AM - 9:00PM '));
-box1.appendChild(createBoxText('<b>Adress</b><br>177A Bleecker Street<br> New York City<br>NY10012-1406'))
-const bookTable = document.createElement('button');
+const box1text = createDOMElement('div', 'textContainer');
+
+box1text.appendChild(createBoxText('<b>Business Hours</b><br>Monday - Sunday<br>10:00AM - 9:00PM '));
+box1text.appendChild(createBoxText('<b>Address</b><br>177A Bleecker Street<br> New York City<br>NY10012-1406'))
+box1.appendChild(box1text);
+const bookTable = createDOMElement("button", "bookTable")
 bookTable.textContent = "BOOK TABLE"
-bookTable.classList.add('bookTable');
 box1.appendChild(bookTable);
 
 
---
+++ src/domFunctions.js
@@ -0,0 +1,16 @@
+function createBoxText(text) {
+    let p = document.createElement('p')
+    p.innerHTML = text;
+    p.classList.add('boxText')
+    return p;
+};
+
+function createDOMElement(type, domClass) {
+    let element = document.createElement(type);
+    if (domClass != "" || domClass != undefined || domClass != null) {
+        element.classList.add(domClass);
+    };
+    return element;
+};
+
+export default { createBoxText, createDOMElement };
