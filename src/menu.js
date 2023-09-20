33e3a3898c04c4c59102b0907a88c0ec9a7aceb8
DR7262 <colina.rbm@gmail.com>
2023-09-20 13:08:43 +1000

    Filled out menu with placeholder items and basic styles

--- src/menu.js
+++ src/menu.js
@@ -1,9 +1,39 @@
 import {createDOMElement} from "./domFunctions.js";
 
+function createMenuItem(name, description, price) {
+    const item = createDOMElement('div', 'menuItem');
+
+    const itemHeader = createDOMElement('div', 'menuItemHeader');
+    const mealName = createDOMElement('p', 'mealName');
+    mealName.innerHTML = name;
+    const mealPrice = createDOMElement('p', 'mealPrice');
+    mealPrice.innerHTML = price;
+
+    const itemBody = createDOMElement('div', 'menuItemBody');
+    const mealDescription = createDOMElement('p', 'mealDescription')
+    mealDescription.innerHTML = description;
+
+    itemHeader.appendChild(mealName);
+    itemHeader.appendChild(mealPrice);
+    itemBody.appendChild(mealDescription);
+
+    item.appendChild(itemHeader);
+    item.appendChild(itemBody);
+
+    return item;
+};
+
 const menuTitle = createDOMElement('div', 'menuTitle');
 menuTitle.textContent = 'Menu';
 
 const menuContainer = createDOMElement('div', 'menu');
+menuContainer.appendChild(createMenuItem('Food Item', 'A delicious piece of edible substance, suitable for nutritional or recreational needs', '$10'));
+menuContainer.appendChild(createMenuItem('Food Item', 'A delicious piece of edible substance, suitable for nutritional or recreational needs', '$10'));
+menuContainer.appendChild(createMenuItem('Food Item', 'A delicious piece of edible substance, suitable for nutritional or recreational needs', '$10'));
+menuContainer.appendChild(createMenuItem('Food Item', 'A delicious piece of edible substance, suitable for nutritional or recreational needs', '$10'));
+menuContainer.appendChild(createMenuItem('Food Item', 'A delicious piece of edible substance, suitable for nutritional or recreational needs', '$10'));
+menuContainer.appendChild(createMenuItem('Food Item', 'A delicious piece of edible substance, suitable for nutritional or recreational needs', '$10'));
+
 
 const menu = [menuTitle, menuContainer]
 
